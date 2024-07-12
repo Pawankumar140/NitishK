6c2972346d55208ed534ad9d08a7cab2ac193e3c
pawankumar93 <pawan.kumar2@hays.com>
2024-07-12 09:40:37 +0530

    added nat and igw

--- terraform/modules/vpc/variable.tf
+++ terraform/modules/vpc/variable.tf
@@ -4,3 +4,9 @@ variable aws_region {}
 variable cidr_block {}
 variable "project" {}
 variable "env" {}
+
+variable "public_subnet" {
+    type = list
+    default = ["172.18.1.0/24", "172.18.2.0/24", "172.18.3.0/24"]
+  
+}
